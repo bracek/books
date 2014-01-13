7cbbf409e7bd7aa1a9efe0039162e18911bc43af
Miroslav Katrak <miroslav.katrak@ixonos.com>
2014-01-13 14:49:53 +0100

    removed comment

--- books/sk-mka-books-heroult-bohatstvo-knihoven/src/main/java/cz/heroult/pavel/bohatstvoKnihoven/kap13/PoradiVMapach.java
+++ books/sk-mka-books-heroult-bohatstvo-knihoven/src/main/java/cz/heroult/pavel/bohatstvoKnihoven/kap13/PoradiVMapach.java
@@ -1,21 +1,5 @@
 package cz.heroult.pavel.bohatstvoKnihoven.kap13;
 
-/////////////////////////////////////////////////////////////////
-//                                                             //
-// Tento zdrojov� k�d je sou��st� distribuce bal�ku program�,  //
-//     poskytovan�ch jako dopl�uj�c� informace ke knize        //
-//                                                             //
-//                   Java -- bohatstv� knihoven                //
-//                II. opraven� a roz���en� vyd�n�              //
-//                                                             //
-//     P�e�t�te si, pros�m, d�kladn� upozorn�n� v souboru      // 
-//                       Cti_me.txt                            //
-//        kter� je ned�lnou sou��st� t�to distribuce           //
-//                                                             //
-//                 (c) Pavel Herout, 2006                      // 
-//                                                             //
-/////////////////////////////////////////////////////////////////
-
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -32,6 +16,7 @@ public class PoradiVMapach {
 	}
 
 	public static void main(final String[] args) {
+
 		nastaveniATisk(new HashMap<String, String>());
 		nastaveniATisk(new TreeMap<String, String>());
 		nastaveniATisk(new LinkedHashMap<String, String>());
