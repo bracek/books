f4fa5e4521c90619c26d15e0690b2cb903ed7808
Miroslav Katrak <miroslav.katrak@ixonos.com>
2014-01-14 08:45:18 +0100

    udpated finalizer to not touch not java classes

--- books/sk-mka-books-heroult/src/main/java/cz/heroult/pavel/java/book/Kap06/S118/Metody5.java
+++ books/sk-mka-books-heroult/src/main/java/cz/heroult/pavel/java/book/Kap06/S118/Metody5.java
@@ -1,28 +1,12 @@
 package cz.heroult.pavel.java.book.Kap06.S118;
 
-/////////////////////////////////////////////////////////////////
-//                                                             //
-// Tento zdrojov� k�d je sou��st� distribuce bal�ku program�,  //
-//     poskytovan�ch jako dopl�uj�c� informace ke knize        //
-//                                                             //
-//                  U�ebnice jazyka Java                       //
-//                                                             //
-//     P�e�t�te si, pros�m, d�kladn� upozorn�n� v souboru      // 
-//                       CTI_ME.TXT                            //
-//        kter� je ned�lnou sou��st� t�to distribuce           //
-//                                                             //
-//                 (c) Pavel Herout, 2000                      // 
-//                                                             //
-/////////////////////////////////////////////////////////////////
-
 public class Metody5 {
-	static double secti(final int a,
-final  double b) {
+	static final double secti(final int a, final double b) {
 		return a + b;
 	}
 
-	static int secti(final int a,
-final  int b) { // p�et�en� metoda secti()
+	static final int secti(final int a, final int b) { // p�et�en� metoda
+														// secti()
 		return a + b;
 	}
 
